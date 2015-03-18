54f83bec1393aad6f61f098cbb5963529d2eb728
matthias <matthias@mucqmaws092>
2015-03-18 12:21:53 +0100

    jpg format is now jpg, fswebcam still uses --jpeg

--- take-snapshot.sh
+++ take-snapshot.sh
@@ -7,8 +7,8 @@ TIMESTAMP_POSITION="bottom" # top or bottom
 TIMESTAMP_FONT="sans" # sans or serif
 TIMESTAMP_SIZE="16"
 TIMESTAMP_COLOR="#00FFFFFF" # #AARRGGBB (Alpha, Red, Green, Blue)
-FORMAT="jpeg" # jpeg or png
-QUALITY="95" # jpeg: 0-95, png: 0-9 (the higher the better)
+FORMAT="jpg" # jpg or png
+QUALITY="95" # jpg: 0-95, png: 0-9 (the higher the better)
 
 TIMELAPSE_HOME="`dirname "$0"`"
 if [ "$TIMELAPSE_HOME" = "." ]; then
@@ -25,12 +25,18 @@ if [ -z "$SNAPSHOTS_DIR" ]; then
 	SNAPSHOTS_DIR="$TIMELAPSE_HOME/snapshots"
 fi
 
+# fswebcam uses --jpeg for the jpg format
+FORMAT_ARG="$FORMAT"
+if [ "$FORMAT_ARG" = "jpg" ]; then
+	FORMAT_ARG="jpeg"
+fi
+
 mkdir -p "$SNAPSHOTS_DIR"
 
 fswebcam --quiet --device "$DEVICE" --resolution "$RESOLUTION" \
 	--$TIMESTAMP_POSITION-banner --banner-colour "#FF000000" --line-colour "#FF000000" \
 	--font "$TIMESTAMP_FONT:$TIMESTAMP_SIZE" --text-colour "$TIMESTAMP_COLOR" --no-shadow \
 	--timestamp "$TIMESTAMP" \
-	--$FORMAT $QUALITY \
+	--$FORMAT_ARG $QUALITY \
 	"$SNAPSHOTS_DIR/snapshot-`date +%Y-%m-%d-%H-%M`.$FORMAT"
 
